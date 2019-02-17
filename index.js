e5c5e7b5e05c0303b69434ab1e9b559795e72d19
Lucille May Atillo <42195338+lucilleatillo29@users.noreply.github.com>
2019-02-18 01:05:17 +0800

    update to latest version

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+//middlewares are wired up to express by the app.use
+//that operates on the incoming requests before they are sent off to our request
 app.use(bodyParser.json());
 app.use(
   cookieSession({
@@ -24,6 +26,21 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+
+if (process.env.NODE_ENV === 'production')
+{
+  //Express will serve up production assets 
+  //like our main.js file or main.css file
+  app.use(express.static('client/build'));
+
+  //Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // app.get('/', (req, res) => {
 //     res.send({ new: 'text'});
 // });
