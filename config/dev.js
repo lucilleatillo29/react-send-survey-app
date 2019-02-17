1081d82faa1d50cae49177194a1351541b68a43f
Lucille May Atillo <42195338+lucilleatillo29@users.noreply.github.com>
2019-02-18 01:03:23 +0800

    update to latest version

---
+++ config/dev.js
@@ -0,0 +1,12 @@
+//console.developers.google.com
+//dev.js - don't commit this!!
+module.exports = {
+  googleClientID:
+    "547013025132-82qf5qolhgqde5auej1trs16dpm687bs.apps.googleusercontent.com",
+  googleClientSecret: "bxvYBZtEJPBU_h6cEcnHpOGu",
+  mongoURI:
+    "mongodb://adminuser:adminuser123@ds011860.mlab.com:11860/emailylucille-dev",
+  cookieKey: "aefiewhfuiwehfuifwefwefwkevergergerger",
+  stripePublishableKey: "pk_test_fTr8JObBXbesI31mIM9lZd4N",
+  stripeSecretKey: "sk_test_tAnJ3BYzOLhYCkWnQXFtRq7n"
+};
